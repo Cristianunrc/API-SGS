14d8fdc321dd4202cc2cb658d009a1683c44e7cf
Cristianunrc <cristiankhan23@gmail.com>
2025-04-14 17:41:11 -0300

    Creating LoginException class

---
+++ apisgs/src/main/java/com/apisgs/apisgs/exceptions/LoginException.java
@@ -0,0 +1,10 @@
+package com.apisgs.apisgs.exceptions;
+
+
+public class LoginException extends RuntimeException {
+  
+  public LoginException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+  
+}
